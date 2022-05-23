20ff4b406ca1ffe36cc03bdac9c67ab9f40f6d71
Henrique Faioli <henrimiguel11@gmail.com>
2022-05-23 14:01:40 -0300

    First commit

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+import "./Footer.css"
+import React from "react"
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido por <strong> Henrique Faioli    </strong><i className="fa fa-heart text-danger"></i>
+        </span>
+    </footer>
